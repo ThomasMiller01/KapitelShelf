ebaf60ff9e06bc19848f5106de20de58912bec85
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-04-22 22:17:41 +0200

    feat: get and create operations

---
+++ backend/src/KapiteShelf.Api/Controllers/BooksController.cs
@@ -0,0 +1,84 @@
+﻿using KapitelShelf.Api.DTOs;
+using KapitelShelf.Api.Logic;
+using KapitelShelf.Api.Settings;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace KapitelShelf.Api.Controllers;
+
+[ApiController]
+[Route("books")]
+public class BooksController : ControllerBase
+{
+    private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    private readonly BooksLogic logic;
+
+    public BooksController(BooksLogic logic)
+    {
+        this.logic = logic;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IList<BookDTO>>> GetBooks()
+    {
+        try
+        {
+            var books = await this.logic.GetBooksAsync();
+            return Ok(books);
+        }
+        catch (Exception ex)
+        {
+            this.logger.Error(ex);
+            return StatusCode(500, new { error = "An unexpected error occurred." });
+        }
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<IList<BookDTO>>> GetBookById(Guid bookId)
+    {
+        try
+        {
+            var book = await this.logic.GetBookByIdAsync(bookId);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+        catch (Exception ex)
+        {
+            this.logger.Error(ex);
+            return StatusCode(500, new { error = "An unexpected error occurred." });
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<BookDTO>> CreateBook(BookDTO bookDto)
+    {
+        try
+        {
+            var book = await this.logic.CreateBook(bookDto);
+
+            return CreatedAtAction(nameof(CreateBook), book);
+        }
+        catch (InvalidOperationException ex)
+        {
+            if (ex.Message == StaticConstants.DuplicateExceptionKey)
+            {
+                return Conflict(new { error = "A book with this title already exists." });
+            }
+
+            // re-throw exception
+            throw;
+        }
+        catch (Exception ex)
+        {
+            this.logger.Error(ex);
+            return StatusCode(500, new { error = "An unexpected error occurred." });
+        }
+    }
+}
---
+++ backend/src/KapiteShelf.Api/Logic/BooksLogic.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using KapitelShelf.Api.DTOs;
+using KapitelShelf.Api.Settings;
+using KapitelShelf.Data;
+using KapitelShelf.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace KapitelShelf.Api.Logic;
+
+public class BooksLogic
+{
+    private readonly IDbContextFactory<KapitelShelfDBContext> dbContextFactory;
+    private readonly IMapper mapper;
+
+    public BooksLogic(IDbContextFactory<KapitelShelfDBContext> dbContextFactory, IMapper mapper)
+    {
+        this.dbContextFactory = dbContextFactory;
+        this.mapper = mapper;
+    }
+
+    public async Task<IList<BookDTO>> GetBooksAsync()
+    {
+        using (var context = await this.dbContextFactory.CreateDbContextAsync())
+        {
+            return await context.Books
+                .AsNoTracking()
+                .Select(x => this.mapper.Map<BookDTO>(x))
+                .ToListAsync();
+        }
+    }
+
+    public async Task<BookDTO?> GetBookByIdAsync(Guid bookId)
+    {
+        using (var context = await this.dbContextFactory.CreateDbContextAsync())
+        {
+            return await context.Books
+                .AsNoTracking()
+                .Where(x => x.Id == bookId)
+                .Select(x => this.mapper.Map<BookDTO>(x))
+                .FirstOrDefaultAsync();
+        }
+    }
+
+    public async Task<BookDTO> CreateBook(BookDTO bookDto)
+    {
+        using (var context = await this.dbContextFactory.CreateDbContextAsync())
+        {
+            var duplicates = await this.GetDuplicates(bookDto);
+            if (duplicates.Any())
+            {
+                throw new InvalidOperationException(StaticConstants.DuplicateExceptionKey);
+            }
+
+            var book = this.mapper.Map<BookModel>(bookDto);
+            book.Id = Guid.NewGuid();
+
+            context.Books.Add(book);
+            await context.SaveChangesAsync();
+
+            return this.mapper.Map<BookDTO>(book);
+        }
+    }
+
+    private async Task<IList<BookModel>> GetDuplicates(BookDTO bookDto)
+    {
+        using (var context = await this.dbContextFactory.CreateDbContextAsync())
+        {
+            var bookDtoSHA = bookDto.Location?.FileInfo?.Sha256;
+
+            return await context.Books
+            .AsNoTracking()
+                .Where(x => x.Title == bookDto.Title)
+                .ToListAsync();
+        }
+    }
+}
