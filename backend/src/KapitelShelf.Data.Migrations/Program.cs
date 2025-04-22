f5d52ab1da002bea390dc336de12eb80ba722f12
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-04-22 19:13:39 +0200

    feat: add db projects

---
+++ backend/src/KapitelShelf.Data.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using KapitelShelf.Data;
+using Microsoft.EntityFrameworkCore;
+
+var options = new DbContextOptionsBuilder<KapitelShelfDBContext>()
+            .UseNpgsql("Host=localhost;Port=5432;Database=kapitelshelf;Username=kapitelshelf;Password=kapitelshelf")
+            .Options;
+
+using var context = new KapitelShelfDBContext(options);
+
+context.Database.Migrate();
+
+Console.WriteLine("Migrations applied.");
---
+++ backend/src/KapitelShelf.Data/KapitelShelfDBContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KapitelShelf.Data;
+
+public class KapitelShelfDBContext : DbContext
+{
+    public KapitelShelfDBContext(DbContextOptions<KapitelShelfDBContext> options) : base(options) { }
+}
