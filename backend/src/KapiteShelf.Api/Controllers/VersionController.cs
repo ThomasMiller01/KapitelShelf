ac0848caa12f329e4ca991aa2b5607ba8d99cc57
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-04-23 16:01:34 +0200

    feat: version endpoint

---
+++ backend/src/KapiteShelf.Api/Controllers/VersionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace KapitelShelf.Api.Controllers;
+
+[ApiController]
+[Route("version")]
+public class VersionController : ControllerBase
+{
+    private readonly ILogger<BooksController> logger;
+
+    public VersionController(ILogger<BooksController> logger)
+    {
+        this.logger = logger;
+    }
+
+    [HttpGet]
+    public ActionResult<string> GetVersion()
+    {
+        try
+        {
+            var informationalVersion = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            var version = informationalVersion?.Split("+")[0];
+            return Ok(version);
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Error parsing version");
+            return StatusCode(500, new { error = "An unexpected error occurred." });
+        }
+    }
+}
