843ccfedd51b9eacec8a92d85181c58dcbcd5050
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-04-30 21:30:29 +0200

    refactor: styles

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -15,40 +15,47 @@ import { useSnackbar } from "notistack";
 import type { ReactElement } from "react";
 
 import { ThemeToggle } from "../contexts/ThemeContext";
+import { useMobile } from "../hooks/useMobile";
 import { ResponsiveDrawerAppBar } from "./base/ResponsiveDrawer";
 interface TopAppBarProps {
   open: boolean;
   toggle: () => void;
 }
 
-export const AppBar = ({ open, toggle }: TopAppBarProps): ReactElement => (
-  <ResponsiveDrawerAppBar open={open} toggle={toggle}>
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <TextField label="Search" variant="outlined" size="small" fullWidth />
-      <Stack
-        direction="row"
-        spacing={{ xs: 2, md: 3 }}
-        alignItems="center"
-        ml="30px"
+export const AppBar = ({ open, toggle }: TopAppBarProps): ReactElement => {
+  const { isMobile } = useMobile();
+
+  return (
+    <ResponsiveDrawerAppBar open={open} toggle={toggle}>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
       >
-        <AddBookActions />
-        <ThemeToggle />
-        <Avatar
-          alt="User Avatar"
-          src="/avatar.png"
-          sx={{ width: 32, height: 32 }}
-        />
-      </Stack>
-    </Box>
-  </ResponsiveDrawerAppBar>
-);
+        <TextField label="Search" variant="outlined" size="small" fullWidth />
+        <Stack
+          direction="row"
+          spacing={{ xs: 2, md: 3 }}
+          alignItems="center"
+          ml={isMobile ? "15px" : "30px"}
+        >
+          <AddBookActions />
+          <Box ml={isMobile ? "10px !important" : ""}>
+            <ThemeToggle />
+          </Box>
+          <Avatar
+            alt="User Avatar"
+            src="/avatar.png"
+            sx={{ width: 32, height: 32 }}
+          />
+        </Stack>
+      </Box>
+    </ResponsiveDrawerAppBar>
+  );
+};
 
 const AddBookActions = (): ReactElement => {
   const { enqueueSnackbar } = useSnackbar();
