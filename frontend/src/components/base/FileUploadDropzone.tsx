3637c4dd7860d370201e499af7f5174683a21025
Thomas Miller <info@thomasmiller.info>
2025-05-09 19:39:07 +0200

    feat: import multiples files at once (#134)

--- frontend/src/components/base/FileUploadDropzone.tsx
+++ frontend/src/components/base/FileUploadDropzone.tsx
@@ -8,7 +8,7 @@ import {
   useTheme,
 } from "@mui/material";
 import { useSnackbar } from "notistack";
-import { type ReactElement, useEffect, useMemo, useState } from "react";
+import { type ReactElement, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
 
 export const baseStyle = (theme: Theme): SxProps<Theme> => ({
@@ -42,51 +42,57 @@ export const rejectStyle = (theme: Theme): SxProps<Theme> => ({
 
 interface FileUploadDropzoneProps {
   onFileChange?: (file: File) => void;
+  onFilesChange?: (files: File[]) => void;
   accept?: string[];
   height?: string;
 }
 
 const FileUploadDropzone = ({
   onFileChange,
+  onFilesChange,
   accept = [],
   height = "fit-content",
 }: FileUploadDropzoneProps): ReactElement => {
   const { enqueueSnackbar } = useSnackbar();
   const theme = useTheme();
 
-  const [currentFile, setCurrentFile] = useState<File>();
-  useEffect(() => {
-    if (currentFile === undefined || currentFile === null) {
+  const onDrop = (files: File[]): void => {
+    if (files === undefined || files === null || files.length === 0) {
       return;
     }
 
-    if (
-      accept.length > 0 &&
-      !accept.includes(currentFile.type) &&
-      accept.filter((x) => currentFile.name.endsWith(x)).length === 0
-    ) {
-      enqueueSnackbar(`Invalid file type, allowed: ${accept.join(", ")}`, {
-        variant: "warning",
-      });
+    const acceptedFiles: File[] = [];
+    files.forEach((file) => {
+      if (
+        accept.length > 0 &&
+        !accept.includes(file.type) &&
+        accept.filter((x) => file.name.endsWith(x)).length === 0
+      ) {
+        enqueueSnackbar(`Invalid file type, allowed: ${accept.join(", ")}`, {
+          variant: "warning",
+        });
+        return;
+      }
 
-      setCurrentFile(undefined);
-      return;
-    }
+      acceptedFiles.push(file);
+    });
 
-    if (onFileChange !== undefined) {
-      onFileChange(currentFile);
+    if (onFilesChange !== undefined) {
+      onFilesChange(acceptedFiles);
+    } else if (onFileChange !== undefined && acceptedFiles.length > 0) {
+      onFileChange(acceptedFiles[0]);
     }
-  }, [currentFile, accept, onFileChange, enqueueSnackbar]);
+  };
 
   const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
     useDropzone({
-      onDrop: (files) => files && setCurrentFile(files[0]),
+      onDrop,
       accept: accept.reduce(
         (dict, key) => ({ [key]: [], ...dict }),
         {} as Record<string, string[]>
       ),
-      maxFiles: 1,
-      multiple: false,
+      maxFiles: onFilesChange !== undefined ? 0 : 1,
+      multiple: onFilesChange !== undefined,
     });
 
   const style = useMemo(
@@ -104,7 +110,8 @@ const FileUploadDropzone = ({
       <input {...getInputProps()} />
       <Stack spacing={4} alignItems="center">
         <Typography textAlign="center" fontSize="1.1rem">
-          Drag & drop a file here, or click to select a file
+          Drag & drop {onFilesChange !== undefined ? "files" : "a file"} here,
+          or click to select {onFilesChange !== undefined ? "files" : "a file"}
         </Typography>
         <CloudUploadIcon sx={{ fontSize: "2.5rem" }} />
       </Stack>
