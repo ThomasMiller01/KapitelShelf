c4ef4f08ce1e32bfd8a1898a5327498d34c50281
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-07-05 16:38:51 +0200

    feat: delete series

--- frontend/src/pages/user/ViewProfilePage.tsx
+++ frontend/src/pages/user/ViewProfilePage.tsx
@@ -2,17 +2,47 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import PeopleIcon from "@mui/icons-material/People";
 import { Box, Container, Divider, Stack, TextField } from "@mui/material";
-import { type ReactElement } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { type ReactElement, useState } from "react";
 import { Link } from "react-router-dom";
 
 import WizardProfile from "../../assets/Wizard.png";
+import DeleteDialog from "../../components/base/feedback/DeleteDialog";
 import { IconButtonWithTooltip } from "../../components/base/IconButtonWithTooltip";
 import ItemAppBar from "../../components/base/ItemAppBar";
 import { useUserProfile } from "../../contexts/UserProfileContext";
+import { usersApi } from "../../lib/api/KapitelShelf.Api";
 import { GetUserColor } from "../../utils/UserProfile";
 
 export const ViewProfilePage = (): ReactElement => {
   const { profile, clearProfile } = useUserProfile();
+
+  const { mutateAsync: mutateDeleteProfile } = useMutation({
+    mutationKey: ["delete-profile", profile?.id],
+    mutationFn: async () => {
+      if (profile?.id === undefined) {
+        return null;
+      }
+
+      await usersApi.usersUserIdDelete(profile.id);
+    },
+    meta: {
+      notify: {
+        enabled: true,
+        operation: "Deleting profile",
+        showLoading: true,
+        showSuccess: true,
+      },
+    },
+  });
+
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const onDelete = async (): Promise<void> => {
+    setDeleteOpen(false);
+
+    await mutateDeleteProfile();
+    clearProfile();
+  };
 
   return (
     <Box>
@@ -36,7 +66,7 @@ export const ViewProfilePage = (): ReactElement => {
             <EditIcon />
           </IconButtonWithTooltip>,
           <IconButtonWithTooltip
-            onClick={() => alert("TODO")}
+            onClick={() => setDeleteOpen(true)}
             key="delete"
             tooltip="Delete Profile"
           >
@@ -73,10 +103,23 @@ export const ViewProfilePage = (): ReactElement => {
               fullWidth
               value={profile?.username}
               disabled
+              sx={{
+                input: {
+                  color: "text.primary",
+                  "-webkit-text-fill-color": "unset !important",
+                },
+              }}
             />
           </Stack>
         </Container>
       </Box>
+      <DeleteDialog
+        open={deleteOpen}
+        onCancel={() => setDeleteOpen(false)}
+        onConfirm={onDelete}
+        title="Confirm to delete this profile"
+        description="Are you sure you want to delete this profile? This action cannot be undone and all data is lost."
+      />
     </Box>
   );
 };
