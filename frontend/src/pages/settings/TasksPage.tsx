0f54597ca8b87bb51c9b97f52fd738c592d3bd58
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-07-11 23:45:42 +0200

    feat: stated task list page

--- frontend/src/pages/settings/TasksPage.tsx
+++ frontend/src/pages/settings/TasksPage.tsx
@@ -1,4 +1,18 @@
 import { Box } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
 import { type ReactElement } from "react";
 
-export const TasksPage = (): ReactElement => <Box>Tasks</Box>;
+import { tasksApi } from "../../lib/api/KapitelShelf.Api";
+
+export const TasksPage = (): ReactElement => {
+  const { data: tasks } = useQuery({
+    queryKey: ["tasks-list"],
+    queryFn: async () => {
+      const { data } = await tasksApi.tasksGet();
+      return data;
+    },
+  });
+  console.log(tasks);
+
+  return <Box>Tasks</Box>;
+};
