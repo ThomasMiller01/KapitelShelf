d4b43ce31dea0ee3501e57e50803cd008d7e7a5a
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-09-17 13:15:12 +0200

    feat: configure backend url for mobile page

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -4,6 +4,7 @@ import { useRoutes } from "react-router-dom";
 import { MainLayout } from "../components/layout/MainLayout";
 import { SettingsLayout } from "../components/layout/SettingsLayout";
 import { useUserProfile } from "../hooks/useUserProfile";
+import { ConfigureBackendUrlPage } from "../mobile/ConfigureBackendUrlPage";
 import BookDetailPage from "../pages/book/BookDetailPage";
 import CreateBookPage from "../pages/book/CreateBookPage";
 import EditBookDetailPage from "../pages/book/EditBookDetailPage";
@@ -20,54 +21,70 @@ import { CreateProfilePage } from "../pages/user/CreateProfilePage";
 import { EditProfilePage } from "../pages/user/EditProfilePage";
 import { SelectProfilePage } from "../pages/user/SelectProfilePage";
 import { ViewProfilePage } from "../pages/user/ViewProfilePage";
+import {
+  IsMobileApiBaseUrlConfigured,
+  IsMobileApp,
+} from "../utils/MobileUtils";
 
 const AppRoutes = (): ReactElement | null => {
   const { profile } = useUserProfile();
 
-  return useRoutes([
-    {
-      path: "/",
-      element: profile === null ? <SelectProfilePage /> : <MainLayout />,
-      children: [
-        { index: true, element: <HomePage /> },
-        { path: "profile", element: <ViewProfilePage /> },
-        { path: "profile/edit", element: <EditProfilePage /> },
-        { path: "library", element: <BooksPage /> },
-        { path: "library/series/:seriesId", element: <SeriesDetailPage /> },
-        {
-          path: "library/series/:seriesId/edit",
-          element: <EditSeriesDetailPage />,
-        },
-        { path: "library/books/:bookId", element: <BookDetailPage /> },
-        { path: "library/books/:bookId/edit", element: <EditBookDetailPage /> },
-        { path: "library/books/create", element: <CreateBookPage /> },
-        { path: "library/books/import", element: <ImportBookPage /> },
-        { path: "search", element: <SearchResultsPage /> },
-        {
-          path: "settings",
-          element: <SettingsLayout />,
-          children: [
-            {
-              index: true,
-              element: <SettingsPage />,
-            },
-            {
-              path: "tasks",
-              element: <TasksPage />,
-            },
-            {
-              path: "cloudstorages",
-              element: <CloudStoragesPage />,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      path: "/create-user-profile",
-      element: <CreateProfilePage />,
-    },
-  ]);
+  return useRoutes(
+    IsMobileApp() && !IsMobileApiBaseUrlConfigured()
+      ? // require mobile app to configure backend URL first, doesnt affect web
+        [{ path: "*", element: <ConfigureBackendUrlPage /> }]
+      : // then show normal routes
+        [
+          {
+            path: "/",
+            element: profile === null ? <SelectProfilePage /> : <MainLayout />,
+            children: [
+              { index: true, element: <HomePage /> },
+              { path: "profile", element: <ViewProfilePage /> },
+              { path: "profile/edit", element: <EditProfilePage /> },
+              { path: "library", element: <BooksPage /> },
+              {
+                path: "library/series/:seriesId",
+                element: <SeriesDetailPage />,
+              },
+              {
+                path: "library/series/:seriesId/edit",
+                element: <EditSeriesDetailPage />,
+              },
+              { path: "library/books/:bookId", element: <BookDetailPage /> },
+              {
+                path: "library/books/:bookId/edit",
+                element: <EditBookDetailPage />,
+              },
+              { path: "library/books/create", element: <CreateBookPage /> },
+              { path: "library/books/import", element: <ImportBookPage /> },
+              { path: "search", element: <SearchResultsPage /> },
+              {
+                path: "settings",
+                element: <SettingsLayout />,
+                children: [
+                  {
+                    index: true,
+                    element: <SettingsPage />,
+                  },
+                  {
+                    path: "tasks",
+                    element: <TasksPage />,
+                  },
+                  {
+                    path: "cloudstorages",
+                    element: <CloudStoragesPage />,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: "/create-user-profile",
+            element: <CreateProfilePage />,
+          },
+        ]
+  );
 };
 
 export default AppRoutes;
