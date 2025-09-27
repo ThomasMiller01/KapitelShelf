ea74d3e10412dee0512f80e9ed29e608a8f94b4b
Thomas Miller <thomas.miller01.mail@gmail.com>
2025-09-27 15:55:51 +0200

    feat: bump readme at frontend version increase

--- frontend/.versionrc.cjs
+++ frontend/.versionrc.cjs
@@ -2,4 +2,15 @@ module.exports = {
   header: "# Changelog\n",
   tagPrefix: "frontend@",
   path: ".",
+  bumpFiles: [
+    {
+      filename: "../README.md",
+      updater: {
+        readVersion: (contents) =>
+          contents.match(/frontend-v(\d+\.\d+\.\d+)/)?.[1] ?? "0.0.0",
+        writeVersion: (contents, version) =>
+          contents.replace(/frontend-v\d+\.\d+\.\d+/g, `frontend-v${version}`),
+      },
+    },
+  ],
 };
