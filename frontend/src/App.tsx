9062f201a9f3e9f2c07b8d84563a65512aa9d126
Thomas Miller <thomas.miller01.mail@gmail.com>
2026-01-13 19:47:07 +0100

    feat: corrected app layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { type ReactElement, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import { CssBaseline, GlobalStyles, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { GlobalPollerCollection } from "./components/poller/GlobalPollerCollection";
 import { ApiProvider } from "./contexts/ApiProvider";
 import { ApiNotificationListener } from "./contexts/notification/ApiNotificationListener";
@@ -15,7 +15,6 @@ import { UserSettingsProvider } from "./contexts/UserSettingsContext";
 import { InitializeMobile } from "./InitializeMobile";
 import AppRoutes from "./routes/routes";
 import { theme } from "./styles/Theme";
-import { IsMobileApp } from "./utils/MobileUtils";
 
 function App(): ReactElement {
   useEffect(() => {
@@ -28,18 +27,6 @@ function App(): ReactElement {
         <UserSettingsProvider>
           <ThemeProvider theme={theme} noSsr>
             <CssBaseline />
-            {/* Make background transparent on mobile apps so mobile navigation/status bars can be transparent */}
-            <GlobalStyles
-              styles={
-                IsMobileApp()
-                  ? {
-                      html: { backgroundColor: "transparent !important" },
-                      body: { backgroundColor: "transparent !important" },
-                      "#root": { backgroundColor: "transparent !important" },
-                    }
-                  : {}
-              }
-            />
             <NotificationProvider>
               <BrowserRouter>
                 <ApiNotificationListener />
