2d2172fad921eb8c35f84e6a8f1b190021aea975
Thomas Miller <thomas.miller01.mail@gmail.com>
2026-01-13 18:30:07 +0100

    fix: make mobile android bottom navigation bar transparent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { type ReactElement, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, GlobalStyles, ThemeProvider } from "@mui/material";
 import { GlobalPollerCollection } from "./components/poller/GlobalPollerCollection";
 import { ApiProvider } from "./contexts/ApiProvider";
 import { ApiNotificationListener } from "./contexts/notification/ApiNotificationListener";
@@ -15,6 +15,7 @@ import { UserSettingsProvider } from "./contexts/UserSettingsContext";
 import { InitializeMobile } from "./InitializeMobile";
 import AppRoutes from "./routes/routes";
 import { theme } from "./styles/Theme";
+import { IsMobileApp } from "./utils/MobileUtils";
 
 function App(): ReactElement {
   useEffect(() => {
@@ -27,6 +28,18 @@ function App(): ReactElement {
         <UserSettingsProvider>
           <ThemeProvider theme={theme} noSsr>
             <CssBaseline />
+            {/* Make background transparent on mobile apps so mobile navigation/status bars can be transparent */}
+            <GlobalStyles
+              styles={
+                IsMobileApp()
+                  ? {
+                      html: { backgroundColor: "transparent !important" },
+                      body: { backgroundColor: "transparent !important" },
+                      "#root": { backgroundColor: "transparent !important" },
+                    }
+                  : {}
+              }
+            />
             <NotificationProvider>
               <BrowserRouter>
                 <ApiNotificationListener />
