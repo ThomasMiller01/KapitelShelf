64679ecd16d5bc03c1f41122e490d30118d18b29
Thomas Miller <thomas.miller01.mail@gmail.com>
2026-01-29 12:44:47 +0100

    fix: use settings poller

--- frontend/src/pages/settings/SettingsPage.tsx
+++ frontend/src/pages/settings/SettingsPage.tsx
@@ -4,11 +4,11 @@ import { type ReactElement } from "react";
 import { AiSettings } from "../../features/settings/ai/AiSettingsFeature";
 import { CloudStorageSettings } from "../../features/settings/CloudStorageSettingsFeature";
 import { MobileSettings } from "../../features/settings/MobileSettingsFeature";
-import { useSettingsList } from "../../lib/requests/settings/useSettingsList";
+import { useSettingsListPoller } from "../../lib/requests/settings/useSettingsListPoller";
 import { IsMobileApp } from "../../utils/MobileUtils";
 
 export const SettingsPage = (): ReactElement => {
-  const { data: settings } = useSettingsList();
+  const { data: settings } = useSettingsListPoller();
 
   return (
     <Box padding="20px">
